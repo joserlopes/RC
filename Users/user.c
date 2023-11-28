4cb7c34677c6813fc86c3674e9985f774302d0e0
José Lopes <josersl2003@gmail.com>
2023-11-28 14:21:08 +0000

    Args parser.

--- Users/user.c
+++ Users/user.c
@@ -7,24 +7,22 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
 #include <time.h>
 #include <dirent.h>
 
 int fd,errcode;
 ssize_t n;
 socklen_t addrlen;
-struct addrinfo hints,*res;
+struct addrinfo hints, *res;
 struct sockaddr_in addr;
 char buffer[128];
 char *AS_addr = "tejo.tecnico.ulisboa.pt";
 // TODO: change the port so it it is 58000+[Group_number]
 // INFO: The port 58001 only echoes the message received, the port 58011 is the actual AS_server
-char *AS_port = "58001";
+char *AS_port = "58011";
 
 int parse_args(int argc, char **argv) {
     if (argc == 3) {
-        puts("TOU AQUI");
         // -n as the only flag
         if (strcmp(argv[1], "-n") == 0) {
             AS_addr = argv[2];
@@ -50,31 +48,63 @@ int parse_args(int argc, char **argv) {
                 AS_addr = argv[4];
             }
         }
+    } else {
+        return EXIT_FAILURE;
     }
+
+    return EXIT_SUCCESS;
+}
+
+int receive_user_input() {
+    char input[300];
+    int integer;
+    float floating_point;
+    char string[20];
+
+    fgets(input, sizeof(input), stdin);
+
+    if (input[strlen(input) - 1] == '\n') {
+        input[strlen(input) - 1] = '\0';
+    }
+
+    int result = sscanf(input, "%d %f %s", &integer, &floating_point, string);
+
+    printf("Result: %d\n", result);
+    printf("Parsed values: %d, %.2f, %s\n", integer, floating_point, string);
+
     return EXIT_SUCCESS;
 }
 
 int main(int argc, char **argv) {
-    parse_args(argc, argv);
-    printf("%s %s\n", AS_addr, AS_port);
+    if (parse_args(argc, argv) == -1)
+        exit(1);
+
     fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) /*error*/ exit(1);
+    if (fd == -1)
+        exit(1);
 
     memset(&hints, 0, sizeof(hints));
 
     hints.ai_family=AF_INET;
     hints.ai_socktype=SOCK_DGRAM;
 
+    printf("%s %s\n", AS_addr, AS_port);
+
     errcode = getaddrinfo(AS_addr, AS_port, &hints, &res);
-    if (errcode != 0) /*error*/ exit(1);
+    if (errcode != 0)
+        exit(1);
 
     n = sendto(fd, "Hello!!!\n", 9, 0, res->ai_addr, res->ai_addrlen);
-    if (n == -1) /* error */ exit(1);
+    if (n == -1)
+        exit(1);
 
     addrlen = sizeof(addr);
-    n = recvfrom(fd, buffer, 128, 0,
-                 (struct sockaddr*)&addr, &addrlen);
-    if (n == -1) /* error */ exit(1);
+
+    n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
+    if (n == -1)
+        exit(1);
+
+    receive_user_input();
 
     write(1, "echo: ", 6);
     write(1, buffer, n);
