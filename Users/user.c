8692c41b160352d707d8ff6f1874cb92e4df197d
José Lopes <josersl2003@gmail.com>
2023-11-30 13:41:08 +0000

    Beggining communication to the server.

--- Users/user.c
+++ Users/user.c
@@ -18,7 +18,16 @@ char buffer[128];
 char *AS_addr = "tejo.tecnico.ulisboa.pt";
 // TODO: change the port so it it is 58000+[Group_number]
 // INFO: The port 58001 only echoes the message received, the port 58011 is the actual AS_server
-char *AS_port = "58001";
+char *AS_port = "58011";
+char input[400];
+char command[20];
+char UID[7];
+char password[9];
+char name[11];
+char asset_fname[30];
+// Estes doubles precisam de ser doubles ou podem ser ints?
+int start_value, time_active;
+int AID;
 
 int parse_args(int argc, char **argv) {
     if (argc == 3) {
@@ -61,20 +70,14 @@ int handle_login(char *input, char *UID, char *password) {
 }
 
 int handle_logout() {
-    puts("Hello!");
-
     return EXIT_SUCCESS;
 }
 
 int handle_unregister() {
-    puts("Hello!");
-
     return EXIT_SUCCESS;
 }
 
 int handle_exit() {
-    puts("Hello!");
-
     return EXIT_SUCCESS;
 }
 
@@ -91,20 +94,14 @@ int handle_close(char *input, int *AID) {
 }
 
 int handle_myauctions() {
-    puts("Hello!");
-
     return EXIT_SUCCESS;
 }
 
 int handle_mybids() {
-    puts("Hello!");
-
     return EXIT_SUCCESS;
 }
 
 int handle_list() {
-    puts("Hello!");
-
     return EXIT_SUCCESS;
 }
 
@@ -127,17 +124,10 @@ int handle_show_record(char *input, int *AID) {
 }
 
 int receive_user_input() {
-    char input[400];
-    char command[20];
-    char UID[7];
-    char password[9];
-    char name[11];
-    char asset_fname[30];
-    // Estes doubles precisam de ser doubles ou podem ser ints?
-    int start_value, time_active;
-    int AID;
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+        fprintf(stderr, "Error: error reading user input");
+    }
 
-    fgets(input, sizeof(input), stdin);
     sscanf(input, "%s", command);
 
     if (!strcmp(command, "login")) {
@@ -173,41 +163,44 @@ int receive_user_input() {
 }
 
 int main(int argc, char **argv) {
-    // if (parse_args(argc, argv) == -1)
-    //     exit(1);
-    //
-    // fd = socket(AF_INET, SOCK_DGRAM, 0);
-    // if (fd == -1)
-    //     exit(1);
-    //
-    // memset(&hints, 0, sizeof(hints));
-    //
-    // hints.ai_family=AF_INET;
-    // hints.ai_socktype=SOCK_DGRAM;
-    //
-    // printf("%s %s\n", AS_addr, AS_port);
-    //
-    // errcode = getaddrinfo(AS_addr, AS_port, &hints, &res);
-    // if (errcode != 0)
-    //     exit(1);
-    //
-    // n = sendto(fd, "Hello!!!\n", 9, 0, res->ai_addr, res->ai_addrlen);
-    // if (n == -1)
-    //     exit(1);
-    //
-    // addrlen = sizeof(addr);
-    //
-    // n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
-    // if (n == -1)
-    //     exit(1);
+    char *new_buffer = "LIN 103938 12345678\n";
+    if (parse_args(argc, argv) == -1)
+        exit(1);
+    
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1)
+        exit(1);
+    
+    memset(&hints, 0, sizeof(hints));
+    
+    hints.ai_family=AF_INET;
+    hints.ai_socktype=SOCK_DGRAM;
+    
+    printf("%s %s\n", AS_addr, AS_port);
+    
+    errcode = getaddrinfo(AS_addr, AS_port, &hints, &res);
+    if (errcode != 0) 
+        exit(1);
 
-    // write(1, "echo: ", 6);
-    // write(1, buffer, n);
-    //
-    // freeaddrinfo(res);
-    // close(fd);
+    while (1) {
+        printf("> ");
+        receive_user_input();
+        if (!strcmp(command, "exit")) {
+            freeaddrinfo(res);
+            close(fd);
+            return EXIT_SUCCESS;
+        }
+        n = sendto(fd, new_buffer, strlen(new_buffer), 0, res->ai_addr, res->ai_addrlen);
+        if (n == -1)
+            exit(1);
 
-    receive_user_input();
+        addrlen = sizeof(addr);
 
-    return EXIT_SUCCESS;
+        n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*)&addr, &addrlen);
+        if (n == -1)
+            exit(1);
+        printf("it's here\n");
+
+        write(1, buffer, n);
+    }
 }
